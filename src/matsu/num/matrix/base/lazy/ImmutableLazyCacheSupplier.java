e4724d0275e382ecbe1864a5d20aa1901e67fdf4
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2024-12-16 22:38:02 +0900

    25.0.6: 遅延初期化支援のリファクタ

--- src/matsu/num/matrix/base/lazy/ImmutableLazyCacheSupplier.java
+++ src/matsu/num/matrix/base/lazy/ImmutableLazyCacheSupplier.java
@@ -5,7 +5,7 @@
  * http://opensource.org/licenses/mit-license.php
  */
 /*
- * 2024.4.4
+ * 2024.12.16
  */
 package matsu.num.matrix.base.lazy;
 
@@ -16,73 +16,71 @@ import java.util.function.Supplier;
  * 返す値が変化しないサプライヤの, 遅延生成とキャッシュの仕組みを提供する.
  * 
  * <p>
- * オブジェクトのコンピュータ(生成器)を与えてサプライヤを生成する. <br>
- * 初めてgetをしたときにコンピュータによりオブジェクトの生成する. <br>
+ * オブジェクトのコンピュータ (作成器) を与えてサプライヤを生成する. <br>
+ * 初めて {@link Supplier#get()} をコールしたときに, コンピュータによりオブジェクトの生成する. <br>
  * 同時にそのオブジェクトをキャッシュしておき,
  * 2回目以降の呼び出しではキャッシュしたオブジェクトを返す.
  * </p>
  * 
  * <p>
- * この仕組みでは, キャッシュが{@code null}であることにより「キャッシュされていない」と判断している. <br>
- * したがって, コンピュータが{@code null}を返す仕組みの場合は毎回コンピュータのgetが呼ばれることに注意せよ.
+ * この仕組みでは, キャッシュが {@code null} であることにより「キャッシュされていない」と判断している. <br>
+ * したがって, コンピュータが {@code null} を返す仕組みの場合は毎回コンピュータのgetが呼ばれることに注意せよ.
  * </p>
  * 
  * @author Matsuura Y.
- * @version 21.0
+ * @version 25.0
+ * @param <T> 生成されるインスタンスの型
  */
-public final class ImmutableLazyCacheSupplier {
+public final class ImmutableLazyCacheSupplier<T> implements Supplier<T> {
 
-    private ImmutableLazyCacheSupplier() {
-        throw new AssertionError();
+    private final Supplier<? extends T> computer;
+
+    //getされるオブジェクト：遅延初期化される
+    private volatile T product;
+
+    //遅延初期化用のロックオブジェクト
+    private final Object lock = new Object();
+
+    /**
+     * @param computer オブジェクトの生成器
+     * @throws NullPointerException 引数にnullが含まれる場合
+     */
+    private ImmutableLazyCacheSupplier(Supplier<? extends T> computer) {
+        this.computer = Objects.requireNonNull(computer);
+    }
+
+    @Override
+    public T get() {
+        T out = this.product;
+        if (Objects.nonNull(out)) {
+            return out;
+        }
+        synchronized (this.lock) {
+            out = this.product;
+            if (Objects.nonNull(out)) {
+                return out;
+            }
+            out = this.computer.get();
+            this.product = out;
+            return out;
+        }
     }
 
     /**
      * 与えられたコンピュータから, このクラスの仕組みを実現するサプライヤを生成する.
      * 
-     * @param <T> サプライヤの型
-     * @param computer コンピュータ
+     * <p>
+     * クラス説明の通り, コンピュータによる計算はキャッシュされる. <br>
+     * したがって,
+     * 引数で与えるサプライヤは可変な状態を持つべきではない.
+     * </p>
+     * 
+     * @param <T> 生成されるインスタンスの型
+     * @param computer コンピュータ: {@code T} 型のインスタンスを作成する機構
      * @return サプライヤ
-     * @throws NullPointerException 引数にnullが含まれる場合
+     * @throws NullPointerException 引数がnullの場合
      */
-    public static <T> Supplier<T> of(Supplier<? extends T> computer) {
-        return new LazyInitializer<>(computer);
+    public static <T> ImmutableLazyCacheSupplier<T> of(Supplier<? extends T> computer) {
+        return new ImmutableLazyCacheSupplier<>(computer);
     }
-
-    private static final class LazyInitializer<T> implements Supplier<T> {
-
-        private final Supplier<? extends T> computer;
-
-        //getされるオブジェクト：遅延初期化される
-        private volatile T product;
-
-        //遅延初期化用のロックオブジェクト
-        private final Object lock = new Object();
-
-        /**
-         * @param computer オブジェクトの生成器
-         * @throws NullPointerException 引数にnullが含まれる場合
-         */
-        LazyInitializer(Supplier<? extends T> computer) {
-            this.computer = Objects.requireNonNull(computer);
-        }
-
-        @Override
-        public T get() {
-            T out = this.product;
-            if (Objects.nonNull(out)) {
-                return out;
-            }
-            synchronized (this.lock) {
-                out = this.product;
-                if (Objects.nonNull(out)) {
-                    return out;
-                }
-                out = this.computer.get();
-                this.product = out;
-                return out;
-            }
-        }
-
-    }
-
 }
