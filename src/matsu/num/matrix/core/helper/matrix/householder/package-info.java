768392ddb989bf51fafd6cd6e8a05fd3378208e9
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-01-18 17:46:57 +0900

    feat: Householderのヘルパに関するパッケージを作成する.
    Householder行列の複雑な生成法を管理できるように, ヘルパ用のパッケージを用意する.
    .helper.matrix.householder

---
+++ src/matsu/num/matrix/core/helper/matrix/householder/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright © 2025 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+/**
+ * Householder行列の実装とその生成について扱うパッケージ.
+ */
+package matsu.num.matrix.core.helper.matrix.householder;
