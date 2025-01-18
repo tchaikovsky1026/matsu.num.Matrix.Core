3d5075aa814f5c4ab6b030890f85cd6323dcce2d
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-01-18 19:55:43 +0900

    docs: Javadocの修正 (matsu.num.matrix.core.sealed 内)

--- src/matsu/num/matrix/core/sealed/DiagonalMatrixSealed.java
+++ src/matsu/num/matrix/core/sealed/DiagonalMatrixSealed.java
@@ -4,6 +4,7 @@
  * This software is released under the MIT License.
  * http://opensource.org/licenses/mit-license.php
  */
+
 /*
  * 2025.1.18
  */
@@ -12,7 +13,9 @@ package matsu.num.matrix.core.sealed;
 import matsu.num.matrix.core.DiagonalMatrix;
 
 /**
- * {@link DiagonalMatrix} の実装をこのモジュール内に制限するためのヘルパーインターフェース.
+ * {@link DiagonalMatrix} の実装をこのモジュール内に制限するためのヘルパーインターフェース. <br>
+ * {@link DiagonalMatrix} の実装を不可視なクラスで実現するため,
+ * 非公開パッケージで継承先を定義する.
  * 
  * @author Matsuura Y.
  */
