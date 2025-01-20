acc25a1ebe4dbc596a94eb62feb870f89631b747
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-01-20 17:34:06 +0900

    feat: sparseパッケージの用意し, 公開する

---
+++ src/matsu/num/matrix/core/sparse/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright © 2025 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * スパースなベクトルとそのファミリーを扱うパッケージ.
+ */
+package matsu.num.matrix.core.sparse;
