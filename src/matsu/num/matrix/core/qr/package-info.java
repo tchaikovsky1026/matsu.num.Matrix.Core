8e012e4732ff01b8bec3fe5d08b6261638e5f0d3
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-06-16 16:17:37 +0900

    add: QR分解を扱うパッケージを追加

---
+++ src/matsu/num/matrix/core/qr/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright © 2025 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+/**
+ * QR分解を扱うパッケージ.
+ */
+package matsu.num.matrix.core.qr;
