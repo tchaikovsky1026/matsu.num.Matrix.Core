7d743a5db1e99996279ca73a59925f904b34b5a9
Matsu <88224610+tchaikovsky1026@users.noreply.github.com>
2026-04-30 13:27:14 +0900

    docs: update Javadoc (#41)

--- src/matsu/num/matrix/core/Matrix.java
+++ src/matsu/num/matrix/core/Matrix.java
@@ -4,6 +4,7 @@
  * This software is released under the MIT License.
  * http://opensource.org/licenses/mit-license.php
  */
+
 /*
  * 2025.6.14
  */
@@ -184,8 +185,7 @@ public interface Matrix {
      * <p>
      * <u>
      * <i>
-     * このメソッドは {@link #transpose()} や
-     * {@link SkeletalAsymmetricMatrix#createTranspose()}
+     * このメソッドは {@link #transpose()}
      * の実装を補助するために用意されている. <br>
      * {@link Matrix} およびそのサブタイプのインスタンスの転置行列を得る場合は,
      * このメソッドではなく {@link #transpose()} を呼ばなければならない.
