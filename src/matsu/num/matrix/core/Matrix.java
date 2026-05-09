5317eb7462b906c1e3abc91ff0bef72a85a3bdd9
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2026-05-09 14:35:15 +0900

    docs: 公開されたcreateTranspose系のヘルパメソッドを,削除予定とする

--- src/matsu/num/matrix/core/Matrix.java
+++ src/matsu/num/matrix/core/Matrix.java
@@ -6,7 +6,7 @@
  */
 
 /*
- * 2025.6.14
+ * 2026.5.9
  */
 package matsu.num.matrix.core;
 
@@ -196,7 +196,11 @@ public interface Matrix {
      * @param original 元の行列
      * @return 転置行列
      * @throws NullPointerException 引数にnullが含まれる場合
+     * @deprecated
+     *                 この公開ヘルパメソッドは version 29 以降に削除される. <br>
+     *                 代替となるメソッドは公開されていない.
      */
+    @Deprecated(forRemoval = true, since = "28.7")
     public static Matrix createTransposedOf(Matrix original) {
         return TranspositionUtil.apply(original);
     }
