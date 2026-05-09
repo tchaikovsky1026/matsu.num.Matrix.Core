e9ce9f7c7f64ec9c97f50267fb860cb0b26af7fe
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2026-05-09 14:44:41 +0900

    docs: createTransposeに関係するヘルパメソッドのJavadocの整理

--- src/matsu/num/matrix/core/helper/matrix/transpose/TranspositionEntryReadableUtil.java
+++ src/matsu/num/matrix/core/helper/matrix/transpose/TranspositionEntryReadableUtil.java
@@ -4,20 +4,22 @@
  * This software is released under the MIT License.
  * http://opensource.org/licenses/mit-license.php
  */
+
 /*
- * 2025.1.7
+ * 2026.5.9
  */
 package matsu.num.matrix.core.helper.matrix.transpose;
 
 import java.util.Objects;
 
 import matsu.num.matrix.core.EntryReadableMatrix;
+import matsu.num.matrix.core.Matrix;
 import matsu.num.matrix.core.MatrixDimension;
 import matsu.num.matrix.core.Symmetric;
 import matsu.num.matrix.core.Vector;
 
 /**
- * {@link EntryReadableMatrix}の転置を扱う.
+ * {@link EntryReadableMatrix} の転置を扱うユーティリティ.
  * 
  * @author Matsuura Y.
  */
@@ -29,8 +31,27 @@ public final class TranspositionEntryReadableUtil {
     }
 
     /**
-     * {@link EntryReadableMatrix} の推奨される実装規約に則った転置行列を返す. <br>
-     * {@link Symmetric} が付与されている場合は, 引数をそのまま返す.
+     * {@link EntryReadableMatrix} の推奨される実装規約に則った転置行列を返す.
+     * 
+     * <p>
+     * 引数 {@code original}, 戻り値 {@code returnValue} について,
+     * {@code returnValue.transpose() == original} が {@code true} である.
+     * <br>
+     * {@code original} に {@link Symmetric} が付与されている場合,
+     * {@code returnValue == original} が {@code true} である.
+     * </p>
+     * 
+     * <p>
+     * <u>
+     * <i>
+     * このメソッドは {@link EntryReadableMatrix#transpose()}
+     * の実装を補助するために用意されている. <br>
+     * {@link Matrix} およびそのサブタイプのインスタンスの転置行列を得る場合は,
+     * このメソッドではなく, インスタンスメソッドである {@link EntryReadableMatrix#transpose()}
+     * を呼ばなければならない.
+     * </i>
+     * </u>
+     * </p>
      *
      * @param original 元の行列
      * @return 転置行列
