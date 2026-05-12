27defa3b2937eee298a9e6b7e87178848c58e399
Matsu <88224610+tchaikovsky1026@users.noreply.github.com>
2026-05-12 22:56:52 +0900

    Feature/issue-47-BaseMatrix 行・列ベクトルの抽出が可能な型を整備 (#48)
    * add: 行列積を実現するcore.multパッケージの用意
    
    * feat: VectorAccessibleMatrix とそのベース実装の用意
    
    * test: BaseVectorAccessibleMatrixのテスト(生成,成分検証,演算)
    
    * add: ProductMatrixの整備
    
    * add: ProductOrthogonalMatrixの整備

---
+++ src/matsu/num/matrix/core/mult/package-info.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright © 2026 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * 行列積の実行により得られるコンポーネントを扱うパッケージ.
+ * 
+ * <p>
+ * 単純な行列積の場合は,
+ * {@link matsu.num.matrix.core.Matrix#multiply(matsu.num.matrix.core.Matrix, matsu.num.matrix.core.Matrix...)
+ * Matrix.multiply(Matrix, Matrix...)}
+ * や
+ * {@link matsu.num.matrix.core.OrthogonalMatrix#multiply(matsu.num.matrix.core.OrthogonalMatrix, matsu.num.matrix.core.OrthogonalMatrix...)
+ * OrthogonalMatrix.multiply(OrthogonalMatrix, OrthogonalMatrix...)}
+ * が適切である. <br>
+ * このパッケージでは, より複雑 &middot; 高機能な行列積とその結果を提供する.
+ * </p>
+ */
+package matsu.num.matrix.core.mult;
