502a28c7c03de90f69c7d8ee610b3ab1347dabb2
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2026-05-12 18:56:31 +0900

    temp: Javadocコンパイル用

--- src/module-info.java
+++ src/module-info.java
@@ -39,6 +39,7 @@
 module matsu.num.Matrix.Core {
     exports matsu.num.matrix.core;
     exports matsu.num.matrix.core.block;
+    exports matsu.num.matrix.core.mult;
     exports matsu.num.matrix.core.nlsf;
     exports matsu.num.matrix.core.qr;
     exports matsu.num.matrix.core.sparse;
